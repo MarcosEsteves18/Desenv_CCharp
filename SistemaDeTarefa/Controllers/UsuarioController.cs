5a5ec23276377eaeb6bc6b908dc68fb92f68d036
MarcosEsteves18 <vi_ni_esteves18@hotmail.com>
2023-09-22 17:37:12 -0300

    Commit curso youtube

---
+++ SistemaDeTarefa/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefa.Models;
+
+namespace SistemaDeTarefa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+
+            return Ok();
+        }
+    }
+}
