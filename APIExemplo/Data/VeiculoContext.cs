c809103151852479617fc4f124d6e29e9adac792
Marcos Vinicius Esteves <111775524+MarcosEsteves18@users.noreply.github.com>
2023-09-25 20:37:06 -0300

    prova 1 de visual

---
+++ APIExemplo/Data/VeiculoContext.cs
@@ -0,0 +1,14 @@
+﻿using APIExemplo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExemplo.Data
+{
+    public class VeiculoContext: DbContext
+    {
+        //Construtor
+        public VeiculoContext(
+            DbContextOptions<VeiculoContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ APIExemplo/Migrations/20230925230753_depois.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIExemplo.Migrations
+{
+    /// <inheritdoc />
+    public partial class depois : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Placa",
+                table: "Veiculos",
+                type: "longtext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Placa",
+                table: "Veiculos");
+        }
+    }
+}
